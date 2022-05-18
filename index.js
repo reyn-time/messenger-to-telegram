383712c928607f956a99bd2b94a053869dda327b
Ryan Lau <ryanlau394@gmail.com>
2022-05-18 20:51:31 +0100

    Linters

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-"use strict";
-
 import express from "express";
-import { json } from "body-parser";
+import bodyParser from "body-parser";
 
-const app = express().use(json());
+const app = express().use(bodyParser.json());
 
 app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
+
+app.post("/webhook", ({ body }, res) => {
+  // Checks this is an event from a page subscription
+  if (body.object === "page") {
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach((entry) => {
+      // Retrieves
+      const event = entry.messaging[0];
+      console.log(event);
+    });
+
+    res.status(200).send("EVENT_RECEIVED");
+  } else {
+    res.sendStatus(404);
+  }
+});
